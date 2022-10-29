2650aa5bfa488a4f2639ecd0657584f5aaa351ea
mahreztouzi <mahreztouzi@gmail.com>
2022-10-30 00:04:47 +0200

    mon premier projet poster sur github

---
+++ src/pages/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>à propos</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga non
+        explicabo perferendis voluptates aliquid dolores placeat blanditiis
+        delectus suscipit, similique beatae consequuntur deserunt voluptate.
+        Iusto ipsam deleniti porro ipsum fugiat eius illum rerum totam
+        laudantium illo quibusdam, consequuntur molestias nam rem sequi eum
+        beatae nesciunt. Doloribus nemo quisquam quo veritatis pariatur! Enim
+        alias quam praesentium at ea ad accusantium magnam expedita placeat
+        possimus? Repudiandae, natus neque at doloremque mollitia maiores porro
+        nam culpa ea vitae ducimus. Perferendis corporis, quidem cum
+        perspiciatis eum rerum ex animi nobis, at laboriosam porro eveniet
+        praesentium repudiandae unde dignissimos voluptate consequuntur minus,
+        aliquid eius nisi.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+        incidunt debitis saepe, facere nihil consequatur vero sequi, nulla
+        excepturi quaerat dicta quidem necessitatibus, neque quia illo enim
+        autem iure aspernatur repudiandae ipsa in error ad adipisci esse. Quam
+        quidem magni esse saepe. Cum omnis vel, assumenda maxime sequi est
+        consectetur quidem minus! Obcaecati quae nemo beatae reprehenderit neque
+        placeat dolores. Molestiae ducimus officia qui cumque nesciunt, cum
+        assumenda illum earum accusantium nam eveniet delectus, deleniti
+        architecto ex sint repudiandae itaque laudantium suscipit pariatur, et
+        eaque! Corporis quisquam impedit facere ex nostrum error dicta quos
+        minus voluptatem deserunt odio delectus id, tempora, enim illo. A,
+        veritatis id! Hic, blanditiis accusantium! Possimus odit cumque aliquid
+        tempore cum dignissimos, facere consequatur! Doloribus perspiciatis est
+        nihil eaque aperiam, commodi, accusamus cum eligendi repellendus sint
+        aut sequi! Adipisci dolores consectetur officia veniam ex rerum facere
+        sapiente labore ipsa nesciunt? Beatae quo quod doloremque optio
+        laudantium error non fugiat ex ullam nisi, a, ratione, repellendus minus
+        velit sequi perferendis ipsa ut earum nulla. Cupiditate ipsam beatae
+        libero, adipisci amet illo officia sint ratione! Nam blanditiis tenetur
+        repudiandae exercitationem doloremque itaque fugiat, excepturi natus
+        animi, necessitatibus praesentium quisquam expedita in soluta enim quo
+        nulla illum asperiores vero.
+      </p>
+    </div>
+  );
+};
+
+export default About;
